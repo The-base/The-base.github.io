47e262865398eb225adac0686260d68e58b9f4fb
chollid <chrisuga@me.com>
2022-06-14 20:26:50 -0700

    New actions file

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/The-base.github.io/' : ''
+}
 
 module.exports = nextConfig
